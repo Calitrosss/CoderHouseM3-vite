bd3f8fe711e164955830621b340667c14f566469
Carlos Vivas <carsed@hotmail.com>
2023-10-22 18:55:27 -0300

    add async mock and call form item list container

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,11 +1,31 @@
 import { Heading } from "@chakra-ui/react";
 
+import { useState, useEffect } from "react";
+
+import { getProducts } from "../asyncMock";
+import ItemList from "./ItemList";
+
 // eslint-disable-next-line react/prop-types
 const ItemListContainer = ({ greeting }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getProducts()
+      .then((resp) => {
+        setProducts(resp);
+      })
+      .catch((err) => {
+        console.error("🚀 ~ file: ItemListContainer.jsx:17 ~ useEffect ~ err:", err);
+      });
+  }, []);
+
   return (
-    <Heading as={"h1"} size={"lg"} textAlign={"center"} p={4}>
-      {greeting}
-    </Heading>
+    <>
+      <Heading as={"h1"} size={"lg"} textAlign={"center"} p={4}>
+        {greeting}
+      </Heading>
+      <ItemList products={products} />
+    </>
   );
 };
 
