f6ad0ee9d1ce5bb7c4f518ad8715c3e8970ded5d
Carlos Vivas <carsed@hotmail.com>
2023-10-28 23:41:51 -0300

    add cart item viw and remove, checkout and empty cart functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,62 @@
+import { useContext } from "react";
+
+import {
+  Button,
+  Heading,
+  SimpleGrid,
+  Text,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Stack,
+} from "@chakra-ui/react";
+
+import { CartContext } from "../contexts/CartContext";
+import CartItem from "./CartItem";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  const { cart, cartCant, cartTotal, removeItem, clearCart } = useContext(CartContext);
+
+  return (
+    <>
+      <Heading as={"h1"} size={"lg"} textAlign={"center"} p={4}>
+        Tu Carrito
+      </Heading>
+      <SimpleGrid columns={1} spacing={4} maxW={"container.md"} mb={10} mx={"auto"}>
+        {cart.map((p) => {
+          return <CartItem key={p.id} {...p} onClick={removeItem} />;
+        })}
+        <Card direction={{ base: "column", sm: "row" }} variant={"filled"} alignItems={"center"}>
+          <CardHeader>
+            <Heading as={"i"} size="md">
+              Detalle de tu carrito
+            </Heading>
+          </CardHeader>
+          <CardBody>
+            <Stack>
+              <Text as={"b"}>Cantidad de productos: {cartCant}</Text>
+              <Text as={"b"}>Total: ${cartTotal}</Text>
+            </Stack>
+          </CardBody>
+          <CardFooter>
+            <Button colorScheme="blue" isDisabled={!cartCant}>
+              Finalizar Compra
+            </Button>
+          </CardFooter>
+        </Card>
+        <Button
+          colorScheme="red"
+          maxW={"fit-content"}
+          margin="auto"
+          onClick={clearCart}
+          isDisabled={!cartCant}
+        >
+          Vaciar carrito
+        </Button>
+      </SimpleGrid>
+    </>
+  );
 };
 
 export default Cart;
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types */
+import { Card, CardBody, CardFooter, Image, Heading, Text, Button } from "@chakra-ui/react";
+import { DeleteIcon } from "@chakra-ui/icons";
+
+const CartItem = ({ id, title, price, pictureUrl, quantity, onClick }) => {
+  return (
+    <Card direction={{ base: "column", sm: "row" }} overflow="hidden" variant="outline">
+      <Image objectFit="cover" maxW={{ base: "100%", sm: "200px" }} src={pictureUrl} alt={title} />
+
+      <CardBody display={"flex"} flexDirection={"column"} justifyContent={"center"}>
+        <Heading size="md">{title}</Heading>
+        <Text py="2">Precio: ${price}</Text>
+        <Text py="2">Cantidad: {quantity}</Text>
+      </CardBody>
+
+      <CardFooter alignItems={"center"}>
+        <Button
+          variant="solid"
+          colorScheme="blue"
+          leftIcon={<DeleteIcon />}
+          onClick={() => onClick(id)}
+        >
+          Quitar
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default CartItem;
