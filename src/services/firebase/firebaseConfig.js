e3c51ffc42685932908190ff1269db17b85100db
Carlos Vivas <carsed@hotmail.com>
2023-10-31 20:26:28 -0300

    add config file for firebase connection

---
+++ src/services/firebase/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD9BZPUQUjqIz70sgnh0Xj_iu-gE9iV9Io",
+  authDomain: "coderhousem3.firebaseapp.com",
+  projectId: "coderhousem3",
+  storageBucket: "coderhousem3.appspot.com",
+  messagingSenderId: "1068074333784",
+  appId: "1:1068074333784:web:a6092971c6e7bc3d20bc2d",
+};
+
+const app = initializeApp(firebaseConfig);
